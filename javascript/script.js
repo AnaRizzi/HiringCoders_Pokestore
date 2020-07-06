d6dfef259b1269a52b004270272e8fc5283d45c7
Ana Rizzi <ana.rizzi.eu@gmail.com>
2020-07-05 23:29:56 -0300

    inserção do carrinho de compras com quantidades e valores

--- javascript/script.js
+++ javascript/script.js
@@ -4,8 +4,9 @@ const pokemons = document.getElementById('pokemons');
 let apiData;
 let infoPokemon;
 let apiDataDetalhes;
-let carrinho = document.getElementById("carrinho");
+let carrinho = document.getElementById("itensCarrinho");
 let precoTotal = document.getElementById("precoTotal");
+let btnFinalizar = document.getElementById("finalizar");
 let precoExibido = 0;
 
 //Para receber a API
@@ -144,44 +145,85 @@ const criarcards = () => {
 		}
 
 		comprar.onclick = () => {
-			let item = document.createElement('div');
-			item.setAttribute('class', 'margem_baixo item_carrinho');
-
-			let item_img = document.createElement('img');
-			item_img.setAttribute('class', 'img_item');
-			item_img.setAttribute('src', imgAtual);
-			item_img.setAttribute('align', 'left');
-
-			let item_nome = document.createElement('p');
-			item_nome.innerHTML = name;
-
-			let item_qtd = document.createElement('input');
-			item_qtd.setAttribute("type", "number");
-			item_qtd.setAttribute('class', 'input_item');
-			item_qtd.value = 1;
-
-			let item_preco = document.createElement('p');
-			item_preco.setAttribute('class', 'preco_item');
-			item_preco.innerHTML = `R$${preco_pok * item_qtd.value},00`;
-
-			let hr = document.createElement('hr');
-
-			let btnremove = document.createElement('button');
-			btnremove.setAttribute('class', 'btn btn-remover bg-transparent');
-			btnremove.innerHTML = 'X';
-
-			precoExibido += preco_pok;
-			precoTotal.innerHTML = `Total: R$${precoExibido},00`;
-
-			item.append(item_img);
-			item.append(item_nome);
-			item.append(item_qtd);
-			item.append(item_preco);
-			item.append(btnremove);
-			item.append(hr);
-			carrinho.append(item);
+			inserirNoCarrinho(imgAtual, name, preco_pok);
 		}
+
+		if (precoExibido === 0){
+			btnFinalizar.style.display = 'none';
+		}
+		else{
+			btnFinalizar.style.display = 'block';
+		}
+
 	})
+}
+
+const inserirNoCarrinho = (imgAtual, name, preco_pok) => {
+	let item = document.createElement('div');
+	item.setAttribute('class', 'margem_baixo item_carrinho');
+
+	let item_img = document.createElement('img');
+	item_img.setAttribute('class', 'img_item');
+	item_img.setAttribute('src', imgAtual);
+	item_img.setAttribute('align', 'left');
+
+	let item_nome = document.createElement('p');
+	item_nome.innerHTML = name;
+
+	let item_qtd = document.createElement('input');
+	item_qtd.setAttribute("type", "number");
+	item_qtd.setAttribute('class', 'input_item');
+	item_qtd.value = 1;
+
+	let item_preco = document.createElement('p');
+	item_preco.setAttribute('class', 'preco_item');
+	item_preco.innerHTML = `R$${preco_pok * item_qtd.value},00`;
+
+	let hr = document.createElement('hr');
+
+	let btnremove = document.createElement('button');
+	btnremove.setAttribute('class', 'btn btn-remover bg-transparent');
+	btnremove.innerHTML = 'X';
+
+	let precoParcial = preco_pok * item_qtd.value
+	precoExibido += precoParcial;
+	precoTotal.innerHTML = `Total: R$${precoExibido},00`;
+
+	item.append(item_img);
+	item.append(item_nome);
+	item.append(item_qtd);
+	item.append(item_preco);
+	item.append(btnremove);
+	item.append(hr);
+	carrinho.append(item);
+
+	if (precoExibido === 0){
+		btnFinalizar.style.display = 'none';
+	}
+	else{
+		btnFinalizar.style.display = 'block';
+	}
+
+	btnremove.onclick = () => {
+		item.remove();
+		precoExibido -= precoParcial
+		precoTotal.innerHTML = `Total: R$${precoExibido},00`;
+
+		if (precoExibido === 0){
+			btnFinalizar.style.display = 'none';
+		}
+		else{
+			btnFinalizar.style.display = 'block';
+		}
+	}
+
+	item_qtd.onchange = () => {
+		precoExibido -= precoParcial;
+		precoParcial = preco_pok * item_qtd.value;
+		precoExibido += precoParcial;
+		item_preco.innerHTML = `R$${preco_pok * item_qtd.value},00`;
+		precoTotal.innerHTML = `Total: R$${precoExibido.toFixed(2)}`;
+	}
 }
 
 
